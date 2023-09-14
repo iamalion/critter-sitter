997045204855bb264ff5009a72119ba662f12af3
Lindsay Warr <iamalion@gmail.com>
2023-09-14 15:29:09 -0700

    Implement and pass first test

--- critter-sitter/src/components/PetProfile.js
+++ critter-sitter/src/components/PetProfile.js
@@ -1,10 +1,30 @@
 import React from 'react';
 
 function PetProfile({}) {
-  return (
-    <div data-testid="pet-profile-1">
-        Hello world!
-    </div>
+ const [petInfo, setPetInfo] = React.useState(
+        {
+            name: '',
+            type: '',
+            avatar: '',
+            birthday: '',
+            breed: '',
+            weight: '',
+            microchip: '',
+            license: '',
+            insurance: '',
+            funFact: '',
+        }
+    );
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setPetInfo({ ...petInfo, [name]: value });
+    };
+
+    return (
+        <div data-testid="pet-profile-1">
+            <h2>Let's talk about your buddy!</h2>
+        </div>
     )
 }
 export default PetProfile;
