4d8d1d3c286fc424c3647fec6222c1d49aff8f93
Lindsay Warr <iamalion@gmail.com>
2023-09-22 11:27:34 -0700

    Refactor ConfirmationInput

---
+++ critter-sitter/src/components/Confirmation.js
@@ -0,0 +1,19 @@
+import React from "react"; 
+
+function Confirmation({ petInfo }) {
+    return (
+        <>
+        <h2>Here's what we have so far:</h2>
+        <img className="avatar" src={petInfo.avatar} alt="Selected Avatar"></img>
+            <p>{petInfo.name} the {petInfo.species}</p>
+            <p>Birthday: {petInfo.birthdayMonth}/{petInfo.birthdayYear}</p>
+            <p>Microchip: {petInfo.microchip ? petInfo.microchip : `n/a`}</p>
+            <p>Insurance: {petInfo.insurance ? petInfo.insurance : `n/a`}</p>
+            <p>Fun Fact: {petInfo.funFact ? petInfo.funFact : `${petInfo.name} has so many fun facts I can't pick one!`}</p>
+            <p>If this is correct, go ahead and click submit! (Don't worry, you can alway update things later!)</p>
+            <button type="submit">Submit</button>
+        </>
+    )
+}
+
+export default Confirmation;
--- critter-sitter/src/components/PetProfile.js
+++ critter-sitter/src/components/PetProfile.js
@@ -7,6 +7,7 @@ import BirthdayInput from './BirthdayInput';
 import MicrochipInput from './MicrochipInput';
 import InsuranceInput from './InsuranceInput';
 import FunFact from './FunFact';
+import Confirmation from './Confirmation';
 
 const initialPetInfo = {
     name: '',
@@ -137,15 +138,7 @@ function PetProfile() {
     {label: 'Confirm and Submit',
     component: (
         <div>
-            <h2>Here's what we have so far:</h2>
-            <img className="avatar" src={petInfo.avatar} alt="Selected Avatar"></img>
-            <p>{petInfo.name} the {petInfo.species}</p>
-            <p>Birthday: {petInfo.birthdayMonth}/{petInfo.birthdayYear}</p>
-            <p>Microchip: {petInfo.microchip ? petInfo.microchip : `n/a`}</p>
-            <p>Insurance: {petInfo.insurance ? petInfo.insurance : `n/a`}</p>
-            <p>Fun Fact: {petInfo.funFact ? petInfo.funFact : `${petInfo.name} has so many fun facts I can't pick one!`}</p>
-            <p>If this is correct, go ahead and click submit! (Don't worry, you can alway update things later!)</p>
-            <button type="submit">Submit</button>
+            <Confirmation petInfo={petInfo} />
         </div>
         )
     },
@@ -154,7 +147,7 @@ function PetProfile() {
   return (
     <>
       <div>
-        <h2>Let's talk about your buddy!</h2>
+        <h2>Let's talk about your critter!</h2>
         <form onSubmit={handleSubmit}>
           {steps[step - 1] && (
             <>
